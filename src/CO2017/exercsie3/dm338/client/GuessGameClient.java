c427b1d2b502d405e9b83f1a5af7fc5d4ad74810
dylanmorroll <dylanjmmorroll@gmail.com>
2016-05-03 14:49:13 +0100

    printing gameOver and finished() in GameState return different values...

--- src/CO2017/exercsie3/dm338/client/GuessGameClient.java
+++ src/CO2017/exercsie3/dm338/client/GuessGameClient.java
@@ -6,67 +6,100 @@ import java.io.*;
 
 public class GuessGameClient {
 
-    public static void main (String args[]) {
+    // The client class is comprised of a single main method
+    public static void main (String args[]) throws IOException {
         
         // Code to read in arguments from command line
-//        if (args.length != 2) {
-//            System.err.println("Usage: java GuessGameClient <host> <port>");
-//            System.exit(1);
-//        }
-//        
-//        String servername = args[0];
-//        int port = Integer.parseInt(args[1]);
+        if (args.length != 2) {
+            System.err.println("Usage: java GuessGameClient <host> <port>");
+            System.exit(1);
+        }
+        
+        // Assign arguments to variables
+        String servername = args[0];
+        int port = Integer.parseInt(args[1]);
 
-        String servername = "localhost";
-        int port = 8080;
-
+        // Try and make a connection to the server
         try (Socket server = new Socket(servername, port)) {
-            
-            System.out.println("Connected to " + server.getInetAddress());
 
+            // Create server and user I/O streams
             BufferedReader serverIn =
                 new BufferedReader (
                     new InputStreamReader(server.getInputStream(), "UTF-8"));
-
             BufferedReader userIn =
                 new BufferedReader(new InputStreamReader(System.in));
-
             Writer serverOut =
                 new OutputStreamWriter(server.getOutputStream());
 
-            String initialStartMessage = serverIn.readLine();
-            System.out.println(initialStartMessage);
+            // Retrieve the start message and print out the information
+            String startMsg = serverIn.readLine();
+            String[] parts = startMsg.split(":");
+            System.out.printf("%s: range is 1..%s, time allowed is %ss%n",
+                    // "START"
+                    parts[0],
+                    // The upper limit of the guess
+                    parts[1],
+                    // The time limit of the game
+                    parts[2]);
             
-            String result;
+            // Whether the game is over or not
+            boolean finished;
             
+            // While the game hasn't finished
             do {
                 
+                // Get the guess from the user
                 System.out.print("Enter guess: ");
                 String userInput = userIn.readLine();
-                System.out.println("Guess made");
 
+                // Send the guess to the server
                 serverOut.write(String.format("%s%n", userInput));
                 serverOut.flush();
-                System.out.println("Guess sent");
 
-//                if (num != 999) {
-//                    result = Integer.parseInt(serverIn.readLine());
-//                    System.out.printf("Server says %d x %d = %d%n",
-//                            num,
-//                            num,
-//                            result);
-//                }
+                // Get the result returned by the server
+                String returnedMsg = serverIn.readLine();
+                String[] returnedParts = returnedMsg.split(":");
                 
-                result = serverIn.readLine();
-                System.out.println("Response receieved");
-                System.out.println(result);
+                // Game is over if the returned message starts with WIN or LOSE
+                finished = (returnedParts[0].equals("WIN") || returnedParts[0].equals("LOSE"));
                 
-            } while (!result.startsWith("WIN"));
+                // Print to the user the result of their last guess
+                // If the game isn't over (and it's not an error message)
+                if (!finished && !returnedParts[0].equals("ERR")) {
+                    
+                    // Use this string format
+                    System.out.printf("Turn %s: %s was %s, %.1fs remaining%n",
+                            // The turn number
+                            returnedParts[2],
+                            // The user's input
+                            userInput,
+                            // Whether the guess was high or low
+                            returnedParts[0],
+                            // The time remaining, in seconds
+                            Integer.parseInt(returnedParts[1])/1000.0);
+                
+                // If the game is over, or there was an error with the guess
+                } else {
+                    
+                    // Use this string format
+                    System.out.printf("Turn %s: %s%n",
+                            // The turn number
+                            returnedParts[1],
+                            // Whether the guess was incorrect (ERR) or if they won or lost
+                            returnedParts[0]);
+                }
+                
+            // Keep asking user for inputs until the game finishes
+            } while (!finished);
             
-            System.out.println("Client shutdown");
+            // Close connection after game has handed
             server.close();
+            
+            // Throw IOException "when its all over"
+            throw new IOException();
         }
         
+        // Catch any errors, print out relevant info and exit the program
         catch (UnknownHostException e) {
             System.err.println("Unknown host: "+servername);
             System.err.println(e);
@@ -74,8 +107,7 @@ public class GuessGameClient {
         }
         
         catch (IOException e) {
-            System.err.println(e);
-            System.exit(1);
+            
         }
     }
 }
--- src/CO2017/exercsie3/dm338/server/GameState.java
+++ src/CO2017/exercsie3/dm338/server/GameState.java
@@ -2,6 +2,7 @@ package CO2017.exercsie3.dm338.server;
 
 import java.io.Writer;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 // Class to represent the state of an instance of the guessing game
 public class GameState implements Runnable {
@@ -15,10 +16,20 @@ public class GameState implements Runnable {
     // The target for this game: a random number between MINVAL and maxValue
     private int target;
     
+    // The maximum value the target can be
     private int maxValue;
+    
+    // The time limit of the game
     private long timeLimit;
+    
+    // A writer object to send messages direct to the client
     private Writer output;
+    
+    // The handler running that created this GameState object
     private GuessGameServerHandler handler;
+    
+    // The time at the beginning of the game to calculate relative difference
+    long initialTime;
     
     // The number of guesses so far
     private int numOfGuesses;
@@ -32,22 +43,28 @@ public class GameState implements Runnable {
     
     // The last guess
     private int lastGuess;
+    
+    // Whether the game was won or not
+    private boolean won;
 
-    /*
-     * Setup an instance of the game.
-     * 
-     * mv - the maximum value to be used in the quessing game; guesses will be in the range MINVAL..mv
-     * tl - the time limit (in milliseconds) for the game (ignore until phase 3)
-     * o - output buffer attached to the client playing the game; can be used to directly send messages to the client (ignore until phase 3)
-     * ggsh - the handler that is playing this instance of the game. (Phase 2 onwards)
-     */
-    GameState(int mv, long tl, java.io.Writer o, GuessGameServerHandler ggsh) {
+    // CONSTRUCTORS
+    // Setup an instance of the game.
+    GameState(int mv, long tl, Writer o, GuessGameServerHandler ggsh) {
         
+        // Assign passed variables to local variables
+        // The maximum value to be used in the guessing game; guesses will be in the range MINVAL..mv
         maxValue = mv;
+        // The time limit (in milliseconds) for the game
         timeLimit = tl;
+        // Output buffer attached to the client playing the game; can be used to directly send messages to the client
         output = o;
+        // The handler that is playing this instance of the game. (Phase 2 onwards)
         handler = ggsh;
         
+        // Set the remaining time to the intial time
+        timeRemaining = tl;
+        
+        // Generate a target value
         target = RANDGEN.nextInt(maxValue) + 1;
         
     }
@@ -61,51 +78,95 @@ public class GameState implements Runnable {
      */
     @Override
     public void run() {
-        // TODO Auto-generated method stub
         
-    }
-    
-    // Change the state of the game according to the guess
-    public void guess(int g) {
-        lastGuess = g;
-        numOfGuesses++;
+        // Get the time at the beginning of the game
+        initialTime = TimeUnit.MILLISECONDS.convert(System.nanoTime(), TimeUnit.NANOSECONDS);
         
-        if (g == target) {
+        // Start the timer
+        while (timeRemaining > 0 && !finished()) {
+            
+            // Calculate the remaining time by the relative difference from the initial time (time elapsed)
+            long currentTime = TimeUnit.MILLISECONDS.convert(System.nanoTime(), TimeUnit.NANOSECONDS);
+            long timeElapsed = currentTime - initialTime;
+            // and subtracting it from the time limit
+            timeRemaining = timeLimit - timeElapsed;
+        }
+        
+        // If the time limit ends and the game hasn't been won yet
+        System.out.println(gameOver);
+        System.out.println(finished());
+        if (!finished()) {
+
+            // End the game
             gameOver = true;
+            won = false;
+    
+            // Send a message to the user that the game is over
+            String response = String.format("LOSE:%d%n", numOfGuesses);
+            try {
+                output.write(response);
+                output.flush();
+            
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            
+            // Print information about the guess
+            System.out.printf("%c - (LOSE)--%.1fs/%d%n",
+                    handler.getClientLetter(),
+                    timeRemaining/1000.0,
+                    numOfGuesses);
         }
     }
     
-    /* Short string describing the state of the game based on the time remaining and the most recent guess
-     * 
-     * Returns:
-        LOSE
-            if the game is over because time is up (phase 3 only)
-        WIN
-            if the player has guessed correctly within the time limit 
-        LOW
-            if the game is not over and the last guess was low
-        HIGH
-            if the game is not over and the last guess was high
-        (empty string)
-            if the game has not yet started 
-     */
+    // Change the state of the game according to the guess (validation is performed prior to method call)
+    public void guess(int g) {
+        
+        // Set the last guess and increase the number of guess counter
+        lastGuess = g;
+        numOfGuesses++;
+        
+        // If the guess was correct and there is still time remaining, the user has won the game
+        if (g == target && timeRemaining > 0) {
+            gameOver = true;
+            won = true;
+        }
+    }
+    
+    // Short string describing the state of the game based on the time remaining and the most recent guess
     public String toString() {
         
+        // If a guess has been made
         if (numOfGuesses != 0) {
-            if (lastGuess == target) {
-                return "WIN";
+            
+            // If the game is over
+            if (finished()) { 
                 
+                // If the player won the game, return WIN
+                if (won) {
+                    return "WIN";
+                    
+                // If the player didn't win the game, return LOSE
+                } else {
+                    return "LOSE";
+                }
+             
+            // If the game is not over, and the last guess was lower than the target
+            // return LOW
             } else if (lastGuess < target) {
                 return "LOW";
-                
+            
+            // If the last guess was higher than the target return LOW
             } else if (lastGuess > target) {
                 return "HIGH";
             }
             
+        // If a guess hasn't been made return an empty string
         } else {
             return "";
         }
         
+        // If something goes wrong return error
         return "ERROR";
     }
     
@@ -129,6 +190,4 @@ public class GameState implements Runnable {
     public long getTimeRemaining() {
         return timeRemaining;
     }
-    
-    
 }
